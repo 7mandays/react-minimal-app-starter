65354cc6d51392333f41a19072073533e945593d
Martin Chapcak <chapcak.m@gmail.com>
2016-08-08 18:11:06 +0900

    Fix devToolsExtension loading

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk  from 'redux-thunk'
 import reducer from './reducers/reducers'
 import App from './components/App'
 
+const initialState = {
+    filter: 3,
+    messages: []
+}
+
 let store = createStore(
   reducer,
-  applyMiddleware(thunk)
-  //  && window.devToolsExtension && window.devToolsExtension()
+  initialState,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
 )
 
 render(
